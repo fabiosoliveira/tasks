337088bf7658a2b82929201b8accc3052a6d2309
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-07-31 19:09:02 -0300

    criar taskList

---
+++ src/screens/TaskList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+import todayImage from '../../assets/imgs/today.jpg';
+import commonStyles from '../commonStyles';
+import Task from '../components/Task';
+
+export default props => {
+  const today = moment()
+    .locale('pt-br')
+    .format('ddd, D [de] MMMM');
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground style={styles.background} source={todayImage}>
+        <View style={styles.titleBar}>
+          <Text style={styles.title}>Hoje</Text>
+          <Text style={styles.subtitle}>{today}</Text>
+        </View>
+      </ImageBackground>
+      <View style={styles.taskList}>
+        <Task
+          desc="Comprar Livro"
+          estimateAt={new Date()}
+          doneAt={new Date()}
+        />
+        <Task desc="Ler Livro" estimateAt={new Date()} doneAt={null} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 3,
+  },
+  taskList: {
+    flex: 7,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+});
