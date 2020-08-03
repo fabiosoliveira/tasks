89f96e8137f3439102d65935e02bcf979131130e
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-08-02 21:39:56 -0300

    Swipe right

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
 import Icons from 'react-native-vector-icons/FontAwesome';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
 import commonStyles from '../commonStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default props => {
   const doneOrNotStyle =
@@ -16,16 +24,28 @@ export default props => {
     .locale('pt-br')
     .format('ddd, D [de] MMMM');
 
+  function getRightContent() {
+    return (
+      <TouchableOpacity style={styles.right}>
+        <Icon name="trash" size={30} color="#FFF" />
+      </TouchableOpacity>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
-        <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
-      </TouchableWithoutFeedback>
-      <View>
-        <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-        <Text style={styles.date}>{formattedDate}</Text>
+    <Swipeable renderRightActions={getRightContent}>
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+          <View style={styles.checkContainer}>
+            {getCheckView(props.doneAt)}
+          </View>
+        </TouchableWithoutFeedback>
+        <View>
+          <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+          <Text style={styles.date}>{formattedDate}</Text>
+        </View>
       </View>
-    </View>
+    </Swipeable>
   );
 };
 
@@ -78,5 +98,12 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     color: commonStyles.colors.subText,
     fontSize: 12,
+  },
+  right: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
   },
 });
