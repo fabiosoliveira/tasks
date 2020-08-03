14b42c179f5cf3af93d1d647a115acca8f4e457f
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-08-02 21:55:49 -0300

    swiple left

--- src/components/Task.js
+++ src/components/Task.js
@@ -32,8 +32,19 @@ export default props => {
     );
   }
 
+  function getLeftContent() {
+    return (
+      <View style={styles.left}>
+        <Icon name="trash" size={20} color="#FFF" style={styles.excludeIcon} />
+        <Text style={styles.excludeTest}>Excluir</Text>
+      </View>
+    );
+  }
+
   return (
-    <Swipeable renderRightActions={getRightContent}>
+    <Swipeable
+      renderRightActions={getRightContent}
+      renderLeftActions={getLeftContent}>
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
           <View style={styles.checkContainer}>
@@ -68,6 +79,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center',
     paddingVertical: 10,
+    backgroundColor: '#FFF',
   },
   checkContainer: {
     width: '20%',
@@ -105,5 +117,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
     paddingHorizontal: 20,
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  excludeIcon: {
+    marginLeft: 10,
+  },
+  excludeTest: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    margin: 10,
   },
 });
